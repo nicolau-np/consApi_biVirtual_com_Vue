b61cc356f3aed2028f7db1f06f902cc575e4a70d
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-13 11:02:12 +0100

    passando a codificacao dos leitors para o componente de leitors

--- src/App.vue
+++ src/App.vue
@@ -6,28 +6,9 @@
 
 <script>
 import Leitors from './components/Leitors.vue'
-import axios from 'axios'
+
 export default {
   name: 'App',
-  data(){
-    return {
-      leitors : []
-    }
-  },
-  components: {
-    Leitors
-  },
-  created: function(){
-    this.getLeitors();
-  },
-  methods: {
-    getLeitors(){
-      axios.get("http://localhost:8000/api/leitors").then((response)=>{
-          console.log('pegou a lista dos leitors...')
-          this.leitors = response.data.data;
-      }).catch((e)=>console.log(e))
-    }
-  }
 
 }
 </script>
--- src/components/Leitors.vue
+++ src/components/Leitors.vue
@@ -1,18 +1,39 @@
 <template>
-    <div>
-        <h1>Primeira Pagina</h1>
-        <h4>Hello World</h4>
-    </div>
+  <div>
+    <h1>Primeira Pagina</h1>
+    <h4>Hello World</h4>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'Leitors'
-
-    }
+import axios from 'axios'
+export default {
+  name: "Leitors",
+  data() {
+    return {
+      leitors: [],
+    };
+  },
+  components: {
+    Leitors,
+  },
+  created: function () {
+    this.getLeitors();
+  },
+  methods: {
+    getLeitors() {
+      axios
+        .get("http://localhost:8000/api/leitors")
+        .then((response) => {
+          console.log("pegou a lista dos leitors...");
+          this.leitors = response.data.data;
+        })
+        .catch((e) => console.log(e));
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 </style>
 
