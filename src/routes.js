b6012ee92b63e46a8a3b19e9536a0b3be1adb3d5
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-13 12:09:41 +0100

    terminado criacao de rotas

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,19 @@
 import vue from 'vue'
 
 import Router from 'vue-router'
+
+import Leitors from './components/Leitors'
+
+vue.use(Router)
+
+const routes = [
+    {
+    name: 'home',
+    path: '/',
+    component: Leitors
+    }
+]
+
+const router = new Router({routes})
+
+export default router
