7ea95e0ba8b5fad63ee952ee655b932d803d25b9
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-13 20:28:23 +0100

    trabalhando nos metodos do send file

--- src/components/NewLeitors.vue
+++ src/components/NewLeitors.vue
@@ -3,32 +3,32 @@
     <h3>Novo Leitor</h3>
 
     <div class="form">
-      <form @submit="postData" method="post" enctype="multipart/form-data">
+      <form @submit="send" method="POST" enctype="multipart/form-data">
         <input
           type="text"
           name="nome"
-          v-model="leitors.nome"
+          v-model="nome"
           placeholder="Nome"
         /><br />
         <input
           type="text"
           name="genero"
-          v-model="leitors.genero"
+          v-model="genero"
           placeholder="Genero"
         /><br />
         <input
           type="text"
           name="bairro"
-          v-model="leitors.bairro"
+          v-model="bairro"
           placeholder="Bairro"
         /><br />
         <input
           type="text"
           name="telefone"
-          v-model="leitors.telefone"
+          v-model="telefone"
           placeholder="Telefone"
         /><br />
-        <input type="file" /><br />
+        <input type="file" @change="setFile" /><br />
         <input type="submit" value="Salvar" />
       </form>
     </div>
@@ -36,24 +36,43 @@
 </template>
 
 <script>
-//import axios from "axios";
+import axios from "axios";
 export default {
   data() {
     return {
-      leitors: {
-        nome: null,
-        genero: null,
-        bairro: null,
-        telefone: null,
-        estado: "on",
-        file: null,
-      },
+      nome: null,
+      genero: null,
+      bairro: null,
+      telefone: null,
+      estado: "on",
+      file: null,
     };
   },
   methods: {
-    postData(e) {
+    setFile(e) {
+      var files = e.target.files;
+      this.file = files[0];
+    },
+    send(e) {
       e.preventDefault();
-     
+      let data = new FormData();
+      data.append("foto", this.file);
+      data.append("nome", this.nome);
+      data.append("genero", this.genero);
+      data.append("telefone", this.telefone);
+      data.append("bairro", this.bairro);
+      data.append("estado", "on");
+
+      axios
+        .post("http://localhost:8000/api/leitors/store", data, {
+          headers: {
+            "Content-type": "multipart/form-data",
+          },
+        })
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((e) => console.log(e));
     },
   },
 };
