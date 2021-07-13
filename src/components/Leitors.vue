114be9de2fdf8db5c70d510c4d982a92b3c7d6fa
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-13 11:21:04 +0100

    estilizando o card de leitors

--- src/components/Leitors.vue
+++ src/components/Leitors.vue
@@ -3,27 +3,18 @@
     <h1>Primeira Pagina</h1>
     <h4>Hello World</h4>
 
-    <table border="1">
-        <thead>
-            <tr>
-                <th>Foto</th>
-                <th>Nome</th>
-                <th>Genero</th>
-                <th>Telefone</th>
-                <th>Bairro</th>
+    <div class="card" v-for="leitor in leitors" :key="leitor.id">
+        <img scr="" class="img_profile">
+        <div class="description">
+         <span class="field">Nome: </span> {{leitor.pessoa.nome}}<br/>
+         <span class="field">Gênero: </span> {{leitor.pessoa.genero}}<br/> 
+         <span class="field">Telefone: </span> {{leitor.telefone}}<br/> 
+         <span class="field">Bairro: </span> {{leitor.bairro}}<br/>
+         <a href="">Editar</a>&nbsp;&nbsp;<a href="">Eliminar</a>
+        </div>
+    </div>
 
-            </tr>
-        </thead>
-        <tbody>
-            <tr v-for="leitor in leitors" :key="leitor.id">
-                <td></td>
-                <td>{{ leitor.pessoa.nome }}</td>
-                <td>{{ leitor.pessoa.genero }}</td>
-                <td>{{ leitor.telefone }}</td>
-                <td>{{ leitor.bairro }}</td>
-            </tr>
-        </tbody>
-    </table>
+
   </div>
 </template>
 
@@ -54,5 +45,23 @@ export default {
 </script>
 
 <style scoped>
+.img_profile{
+    height: 5em;
+    width: 40%;
+}
+.description{
+    font-size: 14px;
+    float: right;
+}
+.field{
+    font-weight: bold;
+    color: #42b983;
+    float: left;
+}
+.card{
+    padding:10px;
+    border: 1px solid #ccc;
+    display: block;
+}
 </style>
 
