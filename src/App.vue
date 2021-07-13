074af07babbb4b7d9540bcd78d5c302c9c4e3a76
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-13 12:16:07 +0100

    criando pagina de novo leitor

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    hello
     <router-view> </router-view>
   </div>
 </template>
--- src/components/Page2.vue
+++ src/components/Page2.vue
@@ -6,7 +6,7 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'page2',
   
 }
 </script>
