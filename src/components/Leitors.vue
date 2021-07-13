06eee64e3ee5348af063ff40ad4c601c7b72ce77
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-13 10:40:50 +0100

    criada a pagina Leitors

---
+++ src/components/Leitors.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Primeira Pagina</h1>
+        <h4>Hello World</h4>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Leitors'
+
+    }
+</script>
+
+<style scoped>
+
+</style>
+
