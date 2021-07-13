e1acb8d652ec2a0c7c217e7e32558ff3ec4cc6a9
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-13 13:08:07 +0100

    trabalhando nas fotos

--- src/components/NewLeitors.vue
+++ src/components/NewLeitors.vue
@@ -3,37 +3,68 @@
     <h3>Novo Leitor</h3>
 
     <div class="form">
-      <form @submit="postData" method="post">
-        <input type="text" name="nome" v-model="leitors.nome" placeholder="Nome" /><br/>
-        <input type="text" name="genero" v-model="leitors.genero" placeholder="Genero"/><br/>
-        <input type="text" name="bairro" v-model="leitors.bairro" placeholder="Bairro"/><br/>
-        <input type="text" name="telefone" v-model="leitors.telefone" placeholder="Telefone"/><br/>
-        <input type="submit" value="Salvar">
+      <form @submit="postData" method="post" enctype="multipart/form-data">
+        <input
+          type="text"
+          name="nome"
+          v-model="leitors.nome"
+          placeholder="Nome"
+        /><br />
+        <input
+          type="text"
+          name="genero"
+          v-model="leitors.genero"
+          placeholder="Genero"
+        /><br />
+        <input
+          type="text"
+          name="bairro"
+          v-model="leitors.bairro"
+          placeholder="Bairro"
+        /><br />
+        <input
+          type="text"
+          name="telefone"
+          v-model="leitors.telefone"
+          placeholder="Telefone"
+        /><br />
+        <input type="file" @change="onFileSelected" /><br />
+        <input type="submit" value="Salvar" />
       </form>
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
-        leitors :{
-            nome: null,
-            genero: null,
-            bairro: null, 
-            telefone: null,
-        }
-         
+      leitors: {
+        nome: null,
+        genero: null,
+        bairro: null,
+        telefone: null,
+        foto: null,
+        estado: "on",
+      },
     };
   },
-  methods:{
-      postData(e){
-          console.warn(this.leitors)
-          e.preventDefault();
-
-      }
-  }
+  methods: {
+    postData(e) {
+      e.preventDefault();
+      axios
+        .post("http://localhost:8000/api/leitors/store", this.leitors)
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((e) => console.log(e));
+    },
+    onFileSelected(e) {
+      this.foto = e.target.files[0]
+      console.log(e)
+    },
+  },
 };
 </script>
 
