ed833a508e73b2552b153260f4f1f36b9703044c
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-13 12:37:46 +0100

    terminado o formulario

--- src/components/NewLeitors.vue
+++ src/components/NewLeitors.vue
@@ -1,19 +1,40 @@
 <template>
-<div>
-      <h3>Novo Leitor</h3>
+  <div>
+    <h3>Novo Leitor</h3>
 
-      <div class="form">
-
-      </div>
-</div>
+    <div class="form">
+      <form @submit="postData" method="post">
+        <input type="text" name="nome" v-model="leitors.nome" placeholder="Nome" /><br/>
+        <input type="text" name="genero" v-model="leitors.genero" placeholder="Genero"/><br/>
+        <input type="text" name="bairro" v-model="leitors.bairro" placeholder="Bairro"/><br/>
+        <input type="text" name="telefone" v-model="leitors.telefone" placeholder="Telefone"/><br/>
+        <input type="submit" value="Salvar">
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+        leitors :{
+            nome: null,
+            genero: null,
+            bairro: null, 
+            telefone: null,
+        }
+         
+    };
+  },
+  methods:{
+      postData(e){
+          e.preventDefault();
+          
+      }
+  }
+};
 </script>
 
 <style>
-
 </style>
