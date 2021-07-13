46a3e9f296b3f69e1826cb7ea4011cdbe8e75ed8
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-13 12:00:59 +0100

    criando exemplo de routa

---
+++ src/components/Page2.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    page 2
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
